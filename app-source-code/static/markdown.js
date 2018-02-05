54b147909b08c27f77c45248ef5b7c24c05e1d1a
Christopher Hebert <hebert.christopherj@gmail.com>
2018-02-04 17:56:25 -0800

    indentation: References.js; move markdown to static/markdown.js

---
+++ app-source-code/static/markdown.js
@@ -0,0 +1,9 @@
+export const markdownText = 
+`#Markdown in react-native is so cool!
+
+- You can **emphasize** what you want, or just _suggest it_.
+- You can [link websites](https://twitter.com/Charles_Mangwa)
+- You can add a Gif:
+
+
+![Some GIF](https://media.giphy.com/media/dkGhBWE3SyzXW/giphy.gif)`;
