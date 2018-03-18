ada4f2e3266ada75df41dbc462c9e8914f69c0bb
omalleya <omalleya@oregonstate.edu>
2018-03-17 18:01:30 -0700

    working ejected app for ios

--- app-source-code/components/Menu.js
+++ app-source-code/components/Menu.js
@@ -1,17 +1,19 @@
 import { DrawerNavigator } from 'react-navigation';
+import React from 'react';
+import { View, Text } from 'react-native';
 import CircleOfFifthsScreen from './CircleOfFifthsScreen';
 import { ReferencesContainer } from './References';
 import { CompositionContainer } from './Composition';
 
 const RootDrawer = DrawerNavigator({
   Home: {
-    screen: CircleOfFifthsScreen,
+    screen: <View><Text>Hello</Text></View>, // CircleOfFifthsScreen,
   },
   References: {
-    screen: ReferencesContainer,
+    screen: <View><Text>Hello</Text></View>,
   },
   Compose: {
-    screen: CompositionContainer,
+    screen: <View><Text>Hello</Text></View>,
   },
 });
 
