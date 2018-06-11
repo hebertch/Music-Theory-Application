2f18a3ced0decc869ad46eed8e9112c55a24dbc8
omalleya <omalleya@oregonstate.edu>
2018-06-10 21:53:35 -0700

    fix colors function

--- fifths/src/static/colors.js
+++ fifths/src/static/colors.js
@@ -10,14 +10,14 @@ export const colorArraySelect = (scaleQuality) => {
   if (scaleQuality === 'maj') {
     return [
       '#A84F9D', '#75529D', '#2860A6', '#2C96CF',
-      '#2DAB94', '#96FF2C', '#FEF628' /*root*/, '#FDC530',
+      '#2DAB94', '#96FF2C', '#FEF628' /*root color*/, '#FDC530',
       '#F5933B', '#F0793E', '#E9313F', '#E14B9C', 
     ];
   } else {
     return [
-      '#F5933B', '#F0793E', '#E9313F', '#E14B9C',
-      '#A84F9D', '#75529D', '#2860A6', '#2C96CF',
-      '#2DAB94', '#96FF2C', '#FEF628', '#FDC530',
+      '#F0793E', '#E9313F', '#E14B9C', '#A84F9D',
+      '#75529D', '#2860A6', '#2C96CF' /*root color*/, '#2DAB94',
+      '#96FF2C', '#FEF628', '#FDC530', '#F5933B', 
     ];
   }
 };
