d639f200c418fecddba4fb791b8d473fe6fa5b33
omalleya <omalleya@oregonstate.edu>
2018-06-10 21:58:20 -0700

    menu fix

--- fifths/src/components/Menu.js
+++ fifths/src/components/Menu.js
@@ -57,85 +57,85 @@ const RootDrawer = DrawerNavigator({
 	title: 'References'
     },
     "The Perfect Fifth": {
-	screen: reference_stack("    The Perfect Fifth", PerfectFifthReferenceContainer),
+	screen: reference_stack("The Perfect Fifth", PerfectFifthReferenceContainer),
 	navigationOptions: {
 	    title: "        The Perfect Fifth"
 	}
     },
     "Musical Gravity": {
-	screen: reference_stack("    Musical Gravity", MusicalGravityReferenceContainer),
+	screen: reference_stack("Musical Gravity", MusicalGravityReferenceContainer),
 	navigationOptions: {
 	    title: "        Musical Gravity"
 	}
 	},
     "Circle of Fifths": {
-	screen: reference_stack('    Circle of Fifths', CircleOfFifthsReferenceContainer),
+	screen: reference_stack('Circle of Fifths', CircleOfFifthsReferenceContainer),
 	navigationOptions: {
 		title: '        Circle of Fifths'
 	}
 	},
     "Harmonic Function": {
-	screen: reference_stack("    Harmonic Function", HarmonicFunctionReferenceContainer),
+	screen: reference_stack("Harmonic Function", HarmonicFunctionReferenceContainer),
 	navigationOptions: {
 	    title: "        Harmonic Function"
 	}
     },
     "Keys and Key Signatures": {
-	screen: reference_stack("    Keys and Key Signatures", KeySignaturesReferenceContainer),
+	screen: reference_stack("Keys and Key Signatures", KeySignaturesReferenceContainer),
 	navigationOptions: {
 	    title: "        Keys and Key Signatures"
 	}
     },
     "Diatonic": {
-	screen: reference_stack("    Diatonic", DiatonicReferenceContainer),
+	screen: reference_stack("Diatonic", DiatonicReferenceContainer),
 	navigationOptions: {
 	    title: "        Diatonic"
 	}
     },
     "Tonal Gravity": {
-	screen: reference_stack("    Tonal Gravity", TonalGravityReferenceContainer),
+	screen: reference_stack("Tonal Gravity", TonalGravityReferenceContainer),
 	navigationOptions: {
 	    title: "        Tonal Gravity"
 	}
     },
     "Parallel Keys and Borrowed Chords": {
-	screen: reference_stack("    Parallel Keys and Borrowed Chords", ParallelAndBorrowedReferenceContainer),
+	screen: reference_stack("Parallel Keys and Borrowed Chords", ParallelAndBorrowedReferenceContainer),
 	navigationOptions: {
 	    title: "        Parallel Keys and Borrowed Chords"
 	}
     },
     "Enharmonic Equivalents": {
-	screen: reference_stack("    Enharmonic Equivalents", EnharmonicEquivalentsReferenceContainer),
+	screen: reference_stack("Enharmonic Equivalents", EnharmonicEquivalentsReferenceContainer),
 	navigationOptions: {
 	    title: "        Enharmonic Equivalents"
 	}
     },
     "Scale Degree and Function": {
-	screen: reference_stack("    Scale Degree and Function", ScaleDegreeReferenceContainer),
+	screen: reference_stack("Scale Degree and Function", ScaleDegreeReferenceContainer),
 	navigationOptions: {
 	    title: "        Scale Degree and Function"
 	}
     },
     "Dominant Chords": {
-	screen: reference_stack("    Dominant Chords", DominantChordsReferenceContainer),
+	screen: reference_stack("Dominant Chords", DominantChordsReferenceContainer),
 	navigationOptions: {
 	    title: "        Dominant Chords"
 	}
     },
     "Secondary Dominants": {
-	screen: reference_stack("    Secondary Dominants", SecondaryDominantsReferenceContainer),
+	screen: reference_stack("Secondary Dominants", SecondaryDominantsReferenceContainer),
 	navigationOptions: {
 	    title: "        Secondary Dominants"
 	}
     },
     "Tritone Substitutes": {
-	screen: reference_stack("    Tritone Substitutes", TritoneSubstitutesReferenceContainer),
+	screen: reference_stack("Tritone Substitutes", TritoneSubstitutesReferenceContainer),
 	navigationOptions: {
 	    title: "        Tritone Substitutes"
 	}
     },
     "Diatonic Substitutes": {
-	screen: reference_stack("    Diatonic Substitutes", DiatonicSubstitutesReferenceContainer),
+	screen: reference_stack("Diatonic Substitutes", DiatonicSubstitutesReferenceContainer),
 	navigationOptions: {
 	    title: "        Diatonic Substitutes"
 	}
